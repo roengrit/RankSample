653d49dc6121fb50c73a34587fd4355c75d8f75a
logon.firstclass@gmail.com <logon.firstclass@gmail.com>
2017-07-11 17:22:51 +0700

    Rank By Linq

--- RankSample/Program.cs
+++ RankSample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace RankSample
     {
         static void Main(string[] args)
         {
+                          
+            Dictionary<string, decimal> DictVal = new Dictionary<string, decimal>();
+
+            DictVal.Add("0001", 100.5m);
+            DictVal.Add("0002", 95.5m);
+            DictVal.Add("0003", 100.5m);
+            DictVal.Add("0004", 70.5m);
+            DictVal.Add("0005", 90m);
+
+            var Result = DictVal.GroupBy(x => x.Value).
+                            OrderByDescending(x => x.Key).
+                            Select((p, i) => new { Order = 1 + i, Participant = p }).
+                            SelectMany(g => g.Participant.Select(p => new
+                            {
+                                Amount = p.Value,
+                                Rank = g.Order,
+                                Code = p.Key
+
+                            }))
+                            //.OrderBy(x=>x.Code) // เรียงตามเดิม เอา Comment ออก
+                            ;
+
         }
     }
 }
---
+++ RankSample/SampleNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RankSample
+{
+    class SampleNode
+    {
+        public string Code { get; set; }
+        public double Amount1 { get; set; }
+        public double Amount2 { get; set; }
+        public double Amount3 { get; set; }
+        public double Amount4 { get; set; }
+        public double Average { get; set; }        
+        public int Rank { get; set; }
+    }
+}
