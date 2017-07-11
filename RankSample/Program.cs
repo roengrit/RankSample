4560d96fd183b2eef5acdfb6a9b3dfc9a53f71d7
logon.firstclass@gmail.com <logon.firstclass@gmail.com>
2017-07-11 13:42:20 +0700

    Add project files.

---
+++ RankSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RankSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
